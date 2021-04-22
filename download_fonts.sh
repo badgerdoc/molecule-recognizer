7e863dafc24a8e4eb1ec5b23b29138a662cb13e7
Egor Osinkin <egor_osinkin@epam.com>
2021-04-22 15:47:40 +0000

    Atom image generator

---
+++ download_fonts.sh
@@ -0,0 +1,10 @@
+export FONTS_FOLDER="fonts"
+export ARCHIVE_NAME="fonts_archive.zip"
+
+mkdir $FONTS_FOLDER
+
+cd $FONTS_FOLDER
+gdown "https://drive.google.com/uc?export=download&confirm=zwbh&id=1o1ZM5p3ow2hedYCy7X85trcYj4_8mHO3" -O $ARCHIVE_NAME
+
+unzip -j $ARCHIVE_NAME
+rm -rf $ARCHIVE_NAME
